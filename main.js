5b40388aa4d94a2b6e288512e03d72e9b0e749b5
Abhigyan Prakash <72396135+Abhigyan2010@users.noreply.github.com>
2021-02-09 20:42:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+Webcam.set({
+    width:310,
+    height:300,
+    img_format: 'png',
+    png_quality:100,
+    constraints:{
+    facingMode:"environment"
+    }
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot(data_uri){
+    Webcam.snap(function(data_uri){
+        document.getElementById("snapshot").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+console.log("ml5 version is "+ml5.version);
+classifier=ml5.imageClassifier('MobileNet',modelLoaded);
+function modelLoaded(){
+    console.log("Model Loaded!");
+}
+function Identify_image(){
+    img=document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }else{
+        console.log(results);
+        document.getElementById("object_name").innerHTML=results[0].label;
+    }
+}
